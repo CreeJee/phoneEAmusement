90bb22a2a3ae4a278947d75640151ccf1f2be511
CreeJee <000as000@naver.com>
2016-12-03 13:08:50 +0900

    :large_blue_diamond: set database basic
    :small_blue_diamond: change sql

--- app/src/main/java/com/creejee/phoneeamusement/sqliteManager.java
+++ app/src/main/java/com/creejee/phoneeamusement/sqliteManager.java
@@ -1,15 +1,21 @@
 package com.creejee.phoneeamusement;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.TextUtils;
+
+import java.sql.Blob;
+
 /**
  * Created by my on 2016-12-03.
  */
 
 public class sqliteManager extends SQLiteOpenHelper{
     public SQLiteDatabase _db = null;
+    public Cursor cursor = null;
 
     public sqliteManager(Context context, String name, CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -18,16 +24,20 @@ public class sqliteManager extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         // 새로운 테이블을 생성한다.
         // create table 테이블명 (컬럼명 타입 옵션);
-            db.execSQL("CREATE TABLE FOOD_LIST(" +
-                        " idx INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        " tagInfo BLOB, " +
-                        " tagId TEXT" +
-                        " tagContent BLOB"+ //split data for ','
+            db.execSQL("CREATE TABLE IF NOT EXIST `cardList`(" +
+                        " `idx` INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        " `cardName` TEXT "+
+                        " `tagInfo` BLOB, " +
+                        " `tagId` BLOB" +
+                        " `tagContent` BLOB"+ //split data for ','
                     ");");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        /*
+            when database column is change
+            //but I think it doesn't change :D
+         */
     }
     public void open(){
         _db = getWritableDatabase();
@@ -37,6 +47,24 @@ public class sqliteManager extends SQLiteOpenHelper{
     }
     public void close(){
         _db.close();
+    }
+    public byte[][][] getData(String tableName, String columnArray[]) {
+
+        SQLiteDatabase db = getReadableDatabase();
+        String columnString = "`"+TextUtils.join("` , `",columnArray)+"`";
+        cursor = db.rawQuery("select "+columnString+" from "+"`tableName`", null);
+        byte data[][][] = {};
+        int cursorNum = 0;
+        while(cursor.moveToNext()) {
+            for (int i = 0; i < cursor.getColumnCount(); i++) {
+                data[cursorNum][i] = cursor.getBlob(i);
+            }
+            cursorNum++;
+        }
+        return data;
+    }
+    public void print(){
+
     }
 }
 
